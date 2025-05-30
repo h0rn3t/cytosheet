47c19714dc7beb513c3b7e8165f3e42dd195ee53
Eugene <vamp.null@gmail.com>
2025-05-30 12:09:40 +0300

    Fix Cython variable declarations

--- src/cytosheet/cell.pyx
+++ src/cytosheet/cell.pyx
@@ -1,10 +1,12 @@
 cdef class Cell:
     # attributes declared in cell.pxd
 
-    def __init__(self, str position=None, object value=None):
+    def __init__(self, str position=None, object value=None, object formula=None, object style_id=None):
         self.position = position
         self.value = value
+        self.formula = formula
+        self.style_id = style_id
 
     def __repr__(self):
         """Return a string representation of the Cell."""
-        return f"<Cell position={self.position}, value={self.value}>"
+        return f"<Cell position={self.position}, value={self.value}, formula={self.formula}>"
